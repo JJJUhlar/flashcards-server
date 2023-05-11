155dd8d55fabd56e2425a58ce16161c9bf29ae06
JJJUhlar <jjuhlar@gmail.com>
2023-05-11 10:58:30 +0100

    input strings to llm

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ import openai
 import os
 
 os.environ["OPENAI_API_KEY"] = "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"
-
+# other models include: text-davinci-002, text-curie-001, text-babbage-001, text-ada-001
+# Should build in redundancy w/ other models in case one is down
 
 app = Flask(__name__)
 
 def getFlashcards(text, type="default"):
 
     if type == "default":
-        flashcard_guard = gd.Guard.from_rail('default_flashcards.rail', num_reasks=1)
+        flashcard_guard = gd.Guard.from_rail('./card-rails/default_flashcards.rail', num_reasks=1)
 
         raw_llm_output, validated_output = flashcard_guard(
             openai.Completion.create,
@@ -21,9 +22,10 @@ def getFlashcards(text, type="default"):
             max_tokens=1024,
             temperature=0.3,
         )
+
         return validated_output
     elif type == "acrostic":
-        acrostic_guard = gd.Guard.from_rail('acrostic_keyword.rail', num_reasks=1)
+        acrostic_guard = gd.Guard.from_rail('./card-rails/acrostic_keyword.rail', num_reasks=1)
 
         acrostic = {}
 
@@ -39,7 +41,7 @@ def getFlashcards(text, type="default"):
             letter.upper()
             acrostic[letter] = ""
             if not letter == " ":
-                acrostic_line_guard = gd.Guard.from_rail('acrostic_line.rail', num_reasks=1)
+                acrostic_line_guard = gd.Guard.from_rail('./card-rails/acrostic_line.rail', num_reasks=1)
                 raw_llm_output, acrostic_line = acrostic_line_guard(
                     openai.Completion.create,
                     prompt_params={"text": text, "letter": letter},
@@ -51,7 +53,7 @@ def getFlashcards(text, type="default"):
 
         return acrostic
     elif type == "mcq":
-        mcq_guard = gd.Guard.from_rail('multiple_choice_cards.rail', num_reasks=1)
+        mcq_guard = gd.Guard.from_rail('./card-rails/multiple_choice_cards.rail', num_reasks=1)
 
         raw_llm_output, validated_output = mcq_guard(
             openai.Completion.create,
@@ -64,7 +66,7 @@ def getFlashcards(text, type="default"):
 
 
     elif type == "rhyme":
-        rhyme_guard = gd.Guard.from_rail('rhyme.rail', num_reasks=1)
+        rhyme_guard = gd.Guard.from_rail('./card-rails/rhyme.rail', num_reasks=1)
 
         raw_llm_output, validated_output = rhyme_guard(
             openai.Completion.create,
@@ -87,8 +89,10 @@ def hello():
 @app.route('/flashcards', methods=['POST'])
 def flashcards():
     print('got request')
-    print(request.data)
+
     text = request.json['text']
     type = request.json['type']
-    cards = getFlashcards(text, type)
+    
+    cards = getFlashcards(str(text), str(type))
+    print(cards)
     return jsonify(cards)
