61f462b6e0da7b1599c631bd218ef20f727a88a8
JJJUhlar <jjuhlar@gmail.com>
2023-06-04 16:37:34 +0100

    login with username and password

---
+++ models/users.py
@@ -0,0 +1,75 @@
+import jwt
+from datetime import datetime, timedelta
+import bcrypt
+from database import connection_pool
+import os
+
+from flask import request, abort
+
+
+def authenticate_token(func):
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('Authorization').split()[1]
+        if not token:
+            abort(401)
+        try:
+            payload = jwt.decode(token, os.environ.get('SECRET_KEY'), algorithms=['HS256'])
+            user_id = payload['user_id']
+            username = payload['username']
+        except (jwt.ExpiredSignatureError, jwt.InvalidSignatureError):
+            abort(401, "Token is invalid or expired. Please log in again.")
+
+        return func(*args, **kwargs)
+    wrapper.__name__ = func.__name__ + "_wrapper"
+    return wrapper
+
+
+def generate_auth_token(user_id, username):
+    expiration = datetime.utcnow() + timedelta(minutes=int(os.environ.get('DEFAULT_SESSION_LENGTH')))
+    payload = {
+        'user_id': user_id,
+        'username': username,
+        'exp': expiration
+    }
+
+    token = jwt.encode(payload, os.environ.get('SECRET_KEY'), algorithm='HS256')
+
+    return token
+
+
+def add_user(username, password, email):
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    conn = connection_pool.getconn()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)", (str(username), hashed_password, str(email)))
+    except Exception as e:
+        print('ERROR adding user to database')
+        print(e)
+    finally:
+        conn.commit()
+        cursor.close()
+        connection_pool.putconn(conn)
+
+    return {"message": "created user"}
+
+
+def check_password(username, password):
+    conn = connection_pool.getconn()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT password FROM users WHERE username = %s", (username,))  
+        row = cursor.fetchone()
+        hashed_password = row[0] if row else None
+
+        is_password_valid = bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')) if hashed_password else False
+    finally:
+        cursor.close()
+        connection_pool.putconn(conn)
+
+    return is_password_valid
+
+
+    
+    
